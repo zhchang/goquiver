c6b8c6d2e9008cf0d1f0a7aa4d8516bb492a4999
chang <chang@nextbillion.ai>
2024-03-27 14:17:12 +0900

    feat: add safe

---
+++ safe/map.go
@@ -0,0 +1,43 @@
+package safe
+
+import "sync"
+
+type Map[K comparable, V any] struct {
+	sync.RWMutex
+	m map[K]V
+}
+
+func NewMap[K comparable, V any]() *Map[K, V] {
+	return &Map[K, V]{
+		m: map[K]V{},
+	}
+}
+
+func (m *Map[K, V]) Set(k K, v V) {
+	defer m.Unlock()
+	m.Lock()
+	m.m[k] = v
+}
+
+func (m *Map[K, V]) Get(k K) (V, bool) {
+	defer m.RUnlock()
+	m.RLock()
+	var exists bool
+	var value V
+	value, exists = m.m[k]
+	return value, exists
+}
+
+func (m *Map[K, V]) Has(k K) bool {
+	defer m.RUnlock()
+	m.RLock()
+	var exists bool
+	_, exists = m.m[k]
+	return exists
+}
+
+func (m *Map[K, V]) Delete(k K) {
+	defer m.Unlock()
+	m.Lock()
+	delete(m.m, k)
+}
---
+++ safe/map_test.go
@@ -0,0 +1,23 @@
+package safe
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMap(t *testing.T) {
+	m := NewMap[int, int]()
+	assert.NotNil(t, m)
+	m.Set(1, 100)
+	v, e := m.Get(1)
+	assert.Equal(t, v, 100)
+	assert.Equal(t, e, true)
+	_, e = m.Get(2)
+	assert.Equal(t, e, false)
+	e = m.Has(2)
+	assert.Equal(t, e, false)
+	m.Delete(1)
+	e = m.Has(1)
+	assert.Equal(t, e, false)
+}
