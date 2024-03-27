02652d168fea1b8cb848287917d5e8f88617ecf2
chang <chang@nextbillion.ai>
2024-03-27 17:42:04 +0900

    fix: skip split_test unless input.geojson exists

--- geo/split_test.go
+++ geo/split_test.go
@@ -5,16 +5,22 @@ import (
 	"testing"
 )
 
-func TestXxx(t *testing.T) {
-	var input, output []byte
+func TestSplit(t *testing.T) {
 	var err error
-	if input, err = os.ReadFile("input.geojson"); err != nil {
+	igp := "./input.geojson"
+	ogp := "./output.geojson"
+	if _, err := os.Stat(igp); err != nil {
+		return
+	}
+
+	var input, output []byte
+	if input, err = os.ReadFile(igp); err != nil {
 		panic(err)
 	}
 	if output, err = Split(input, 3, 3); err != nil {
 		panic(err)
 	}
-	if err = os.WriteFile("output.geojson", output, 0644); err != nil {
+	if err = os.WriteFile(ogp, output, 0644); err != nil {
 		panic(err)
 	}
 	//println(string(o))
