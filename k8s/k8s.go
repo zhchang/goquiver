bb678209b3cc24ad3991dca0f055833c7455e80c
chang <chang@nextbillion.ai>
2024-04-04 12:25:28 +0900

    feat: support remove

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -125,7 +125,7 @@ func GenManifest(ctx context.Context, chartPath string, values map[string]any) (
 	return results, nil
 }
 
-func GetPvcs(ctx context.Context, stsName, namespace string) ([]v1.PersistentVolumeClaim, error) {
+func getPvcs(ctx context.Context, stsName, namespace string) ([]*v1.PersistentVolumeClaim, error) {
 	var err error
 	if err = Init(); err != nil {
 		return nil, err
@@ -142,11 +142,11 @@ func GetPvcs(ctx context.Context, stsName, namespace string) ([]v1.PersistentVol
 	for _, vct := range sts.Spec.VolumeClaimTemplates {
 		patterns = append(patterns, fmt.Sprintf("%s-%s-", vct.ObjectMeta.Name, stsName))
 	}
-	var result []v1.PersistentVolumeClaim
+	var result []*v1.PersistentVolumeClaim
 	for _, pvc := range pvcs.Items {
 		for _, pattern := range patterns {
 			if strings.Contains(pvc.Name, pattern) {
-				result = append(result, pvc)
+				result = append(result, &pvc)
 			}
 		}
 	}
@@ -173,7 +173,37 @@ func WithWait(duration time.Duration) OperationOption {
 	}
 }
 
-func waitForIt[T Resource](ctx context.Context, api API[T], name string, duration time.Duration) error {
+func waitForRemove[T Resource](ctx context.Context, api API[T], name string, duration time.Duration) error {
+	done := make(chan error)
+	timeout, cancelFunc := context.WithTimeout(ctx, duration)
+	defer cancelFunc()
+	go func() {
+		defer close(done)
+		var err error
+		var ok bool
+		for {
+			_, err = api.Get(context.Background(), name, metav1.GetOptions{})
+			if err != nil {
+				ok = errors.IsNotFound(err)
+			}
+			if !ok {
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			return
+		}
+	}()
+	select {
+	case err := <-done:
+		return err
+	case <-timeout.Done():
+		return timeout.Err()
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func waitForRollout[T Resource](ctx context.Context, api API[T], name string, duration time.Duration) error {
 	done := make(chan error)
 	timeout, cancelFunc := context.WithTimeout(ctx, duration)
 	defer cancelFunc()
@@ -250,13 +280,12 @@ func newResource[T Resource]() T {
 	return reflect.New(reflect.TypeOf(t).Elem()).Interface().(T)
 }
 
-func rollout[T Resource](ctx context.Context, item Resource, apiGetter APIGetter[T], ops OperationOptions) error {
+func rollout[T Resource](ctx context.Context, item Resource, api API[T], ops OperationOptions) error {
 	var err error
 	var instance T
 	if instance, err = ensureResource[T](item); err != nil {
 		return err
 	}
-	api := apiGetter(instance.GetNamespace())
 	if _, err = api.Create(ctx, instance, metav1.CreateOptions{}); err != nil {
 		if errors.IsAlreadyExists(err) {
 			if _, err = api.Update(ctx, instance, metav1.UpdateOptions{}); err != nil {
@@ -267,15 +296,91 @@ func rollout[T Resource](ctx context.Context, item Resource, apiGetter APIGetter
 		}
 	}
 	if ops.wait > 0 {
-		if err = waitForIt(ctx, api, instance.GetName(), ops.wait); err != nil {
+		if err = waitForRollout(ctx, api, instance.GetName(), ops.wait); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func Delete(ctx context.Context, name, namespace, kind string) error {
-	panic("implement me")
+func remove[T Resource](ctx context.Context, name string, api API[T], ops OperationOptions) error {
+	var err error
+	if err = api.Delete(ctx, name, metav1.DeleteOptions{}); err != nil {
+		return err
+	}
+
+	if ops.wait > 0 {
+		if err = waitForRemove(ctx, api, name, ops.wait); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Remove(ctx context.Context, name, namespace, kind string, options ...OperationOption) error {
+	var err error
+	if err = Init(); err != nil {
+		return err
+	}
+	var ops OperationOptions
+	for _, option := range options {
+		option(&ops)
+	}
+	switch kind {
+	case Deployment:
+		return remove[*appsv1.Deployment](ctx, name, clientset.AppsV1().Deployments(namespace), ops)
+	case StatefulSet:
+		if err = remove[*appsv1.StatefulSet](ctx, name, clientset.AppsV1().StatefulSets(namespace), ops); err != nil {
+			return err
+		}
+		var pvcs []*v1.PersistentVolumeClaim
+		if pvcs, err = getPvcs(ctx, name, namespace); err != nil {
+			return err
+		}
+		for _, pvc := range pvcs {
+			//ignore err here so that we remove all pvcs belong to statefulset in best effort
+			_ = remove[*v1.PersistentVolumeClaim](ctx, pvc.GetName(), clientset.CoreV1().PersistentVolumeClaims(namespace), ops)
+		}
+		return nil
+	case ConfigMap:
+		return remove[*v1.ConfigMap](ctx, name, clientset.CoreV1().ConfigMaps(namespace), ops)
+	case CronJob:
+		return remove[*batchv1.CronJob](ctx, name, clientset.BatchV1().CronJobs(namespace), ops)
+	case Service:
+		return remove[*v1.Service](ctx, name, clientset.CoreV1().Services(namespace), ops)
+	case Ingress:
+		return remove[*networkingv1.Ingress](ctx, name, clientset.NetworkingV1().Ingresses(namespace), ops)
+	case PodDisruptionBudget:
+		return remove[*policyv1.PodDisruptionBudget](ctx, name, clientset.PolicyV1().PodDisruptionBudgets(namespace), ops)
+	case Secret:
+		return remove[*v1.Secret](ctx, name, clientset.CoreV1().Secrets(namespace), ops)
+	case StorageClass:
+		return remove[*storagev1.StorageClass](ctx, name, clientset.StorageV1().StorageClasses(), ops)
+	case PersistentVolumeClaim:
+		return remove[*v1.PersistentVolumeClaim](ctx, name, clientset.CoreV1().PersistentVolumeClaims(namespace), ops)
+	case PersistentVolume:
+		return remove[*v1.PersistentVolume](ctx, name, clientset.CoreV1().PersistentVolumes(), ops)
+	case CustomResourceDefinition:
+		return remove[*apiextensionsv1.CustomResourceDefinition](ctx, name, aeClientset.ApiextensionsV1().CustomResourceDefinitions(), ops)
+	case ServiceAccount:
+		return remove[*v1.ServiceAccount](ctx, name, clientset.CoreV1().ServiceAccounts(namespace), ops)
+	case ClusterRole:
+		return remove[*rbacv1.ClusterRole](ctx, name, clientset.RbacV1().ClusterRoles(), ops)
+	case ClusterRoleBinding:
+		return remove[*rbacv1.ClusterRoleBinding](ctx, name, clientset.RbacV1().ClusterRoleBindings(), ops)
+	case Role:
+		return remove[*rbacv1.Role](ctx, name, clientset.RbacV1().Roles(namespace), ops)
+	case RoleBinding:
+		return remove[*rbacv1.RoleBinding](ctx, name, clientset.RbacV1().RoleBindings(namespace), ops)
+	case DaemonSet:
+		return remove[*appsv1.DaemonSet](ctx, name, clientset.AppsV1().DaemonSets(namespace), ops)
+	case Pod:
+		return remove[*v1.Pod](ctx, name, clientset.CoreV1().Pods(namespace), ops)
+	case Job:
+		return remove[*batchv1.Job](ctx, name, clientset.BatchV1().Jobs(namespace), ops)
+	default:
+		return fmt.Errorf("remove unsupported kind: %s", kind)
+	}
 }
 
 func Rollout(ctx context.Context, item Resource, options ...OperationOption) error {
@@ -290,87 +395,46 @@ func Rollout(ctx context.Context, item Resource, options ...OperationOption) err
 	kind := item.GetObjectKind().GroupVersionKind().Kind
 	switch kind {
 	case Deployment:
-		return rollout[*appsv1.Deployment](ctx, item, func(namespace string) API[*appsv1.Deployment] {
-			return clientset.AppsV1().Deployments(namespace)
-		}, *ops.Clone(true))
+		return rollout[*appsv1.Deployment](ctx, item, clientset.AppsV1().Deployments(item.GetNamespace()), *ops.Clone(true))
 	case StatefulSet:
-		return rollout[*appsv1.StatefulSet](ctx, item, func(namespace string) API[*appsv1.StatefulSet] {
-			return clientset.AppsV1().StatefulSets(namespace)
-		}, *ops.Clone(true))
+		return rollout[*appsv1.StatefulSet](ctx, item, clientset.AppsV1().StatefulSets(item.GetNamespace()), *ops.Clone(true))
 	case ConfigMap:
-		return rollout[*v1.ConfigMap](ctx, item, func(namespace string) API[*v1.ConfigMap] {
-			return clientset.CoreV1().ConfigMaps(namespace)
-		}, *ops.Clone(false))
+		return rollout[*v1.ConfigMap](ctx, item, clientset.CoreV1().ConfigMaps(item.GetNamespace()), *ops.Clone(false))
 	case CronJob:
-		return rollout[*batchv1.CronJob](ctx, item, func(namespace string) API[*batchv1.CronJob] {
-			return clientset.BatchV1().CronJobs(namespace)
-		}, *ops.Clone(false))
+		return rollout[*batchv1.CronJob](ctx, item, clientset.BatchV1().CronJobs(item.GetNamespace()), *ops.Clone(false))
 	case Service:
-		return rollout[*v1.Service](ctx, item, func(namespace string) API[*v1.Service] {
-			return clientset.CoreV1().Services(namespace)
-		}, *ops.Clone(false))
+		return rollout[*v1.Service](ctx, item, clientset.CoreV1().Services(item.GetNamespace()), *ops.Clone(false))
 	case Ingress:
-		return rollout[*networkingv1.Ingress](ctx, item, func(namespace string) API[*networkingv1.Ingress] {
-			return clientset.NetworkingV1().Ingresses(namespace)
-		}, *ops.Clone(false))
+		return rollout[*networkingv1.Ingress](ctx, item, clientset.NetworkingV1().Ingresses(item.GetNamespace()), *ops.Clone(false))
 	case PodDisruptionBudget:
-		return rollout[*policyv1.PodDisruptionBudget](ctx, item, func(namespace string) API[*policyv1.PodDisruptionBudget] {
-			return clientset.PolicyV1().PodDisruptionBudgets(namespace)
-		}, *ops.Clone(false))
+		return rollout[*policyv1.PodDisruptionBudget](ctx, item, clientset.PolicyV1().PodDisruptionBudgets(item.GetNamespace()), *ops.Clone(false))
 	case Secret:
-		return rollout[*v1.Secret](ctx, item, func(namespace string) API[*v1.Secret] {
-			return clientset.CoreV1().Secrets(namespace)
-		}, *ops.Clone(false))
+		return rollout[*v1.Secret](ctx, item, clientset.CoreV1().Secrets(item.GetNamespace()), *ops.Clone(false))
 	case StorageClass:
-		return rollout[*storagev1.StorageClass](ctx, item, func(_ string) API[*storagev1.StorageClass] {
-			return clientset.StorageV1().StorageClasses()
-		}, *ops.Clone(false))
+		return rollout[*storagev1.StorageClass](ctx, item, clientset.StorageV1().StorageClasses(), *ops.Clone(false))
 	case PersistentVolumeClaim:
-		return rollout[*v1.PersistentVolumeClaim](ctx, item, func(namespace string) API[*v1.PersistentVolumeClaim] {
-			return clientset.CoreV1().PersistentVolumeClaims(namespace)
-		}, *ops.Clone(false))
+		return rollout[*v1.PersistentVolumeClaim](ctx, item, clientset.CoreV1().PersistentVolumeClaims(item.GetNamespace()), *ops.Clone(false))
 	case PersistentVolume:
-		return rollout[*v1.PersistentVolume](ctx, item, func(_ string) API[*v1.PersistentVolume] {
-			return clientset.CoreV1().PersistentVolumes()
-		}, *ops.Clone(false))
+		return rollout[*v1.PersistentVolume](ctx, item, clientset.CoreV1().PersistentVolumes(), *ops.Clone(false))
 	case CustomResourceDefinition:
-		return rollout[*apiextensionsv1.CustomResourceDefinition](ctx, item, func(_ string) API[*apiextensionsv1.CustomResourceDefinition] {
-			return aeClientset.ApiextensionsV1().CustomResourceDefinitions()
-		}, *ops.Clone(false))
+		return rollout[*apiextensionsv1.CustomResourceDefinition](ctx, item, aeClientset.ApiextensionsV1().CustomResourceDefinitions(), *ops.Clone(false))
 	case ServiceAccount:
-		return rollout[*v1.ServiceAccount](ctx, item, func(namespace string) API[*v1.ServiceAccount] {
-			return clientset.CoreV1().ServiceAccounts(namespace)
-		}, *ops.Clone(false))
+		return rollout[*v1.ServiceAccount](ctx, item, clientset.CoreV1().ServiceAccounts(item.GetNamespace()), *ops.Clone(false))
 	case ClusterRole:
-		return rollout[*rbacv1.ClusterRole](ctx, item, func(_ string) API[*rbacv1.ClusterRole] {
-			return clientset.RbacV1().ClusterRoles()
-		}, *ops.Clone(false))
+		return rollout[*rbacv1.ClusterRole](ctx, item, clientset.RbacV1().ClusterRoles(), *ops.Clone(false))
 	case ClusterRoleBinding:
-		return rollout[*rbacv1.ClusterRoleBinding](ctx, item, func(_ string) API[*rbacv1.ClusterRoleBinding] {
-			return clientset.RbacV1().ClusterRoleBindings()
-		}, *ops.Clone(false))
+		return rollout[*rbacv1.ClusterRoleBinding](ctx, item, clientset.RbacV1().ClusterRoleBindings(), *ops.Clone(false))
 	case Role:
-		return rollout[*rbacv1.Role](ctx, item, func(namespace string) API[*rbacv1.Role] {
-			return clientset.RbacV1().Roles(namespace)
-		}, *ops.Clone(false))
+		return rollout[*rbacv1.Role](ctx, item, clientset.RbacV1().Roles(item.GetNamespace()), *ops.Clone(false))
 	case RoleBinding:
-		return rollout[*rbacv1.RoleBinding](ctx, item, func(namespace string) API[*rbacv1.RoleBinding] {
-			return clientset.RbacV1().RoleBindings(namespace)
-		}, *ops.Clone(false))
+		return rollout[*rbacv1.RoleBinding](ctx, item, clientset.RbacV1().RoleBindings(item.GetNamespace()), *ops.Clone(false))
 	case DaemonSet:
-		return rollout[*appsv1.DaemonSet](ctx, item, func(namespace string) API[*appsv1.DaemonSet] {
-			return clientset.AppsV1().DaemonSets(namespace)
-		}, *ops.Clone(false))
+		return rollout[*appsv1.DaemonSet](ctx, item, clientset.AppsV1().DaemonSets(item.GetNamespace()), *ops.Clone(false))
 	case Pod:
-		return rollout[*v1.Pod](ctx, item, func(namespace string) API[*v1.Pod] {
-			return clientset.CoreV1().Pods(namespace)
-		}, *ops.Clone(false))
+		return rollout[*v1.Pod](ctx, item, clientset.CoreV1().Pods(item.GetNamespace()), *ops.Clone(false))
 	case Job:
-		return rollout[*batchv1.Job](ctx, item, func(namespace string) API[*batchv1.Job] {
-			return clientset.BatchV1().Jobs(namespace)
-		}, *ops.Clone(false))
-
+		return rollout[*batchv1.Job](ctx, item, clientset.BatchV1().Jobs(item.GetNamespace()), *ops.Clone(false))
 	default:
-		return fmt.Errorf("unsupported kind: %s", kind)
+		return fmt.Errorf("rollou unsupported kind: %s", kind)
 	}
 }
